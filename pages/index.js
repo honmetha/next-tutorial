f6a1b542abbdb5026e55a689684ffb2c62f78cf1
Hon <dennis.mejen@gmail.com>
2021-03-05 23:56:21 +0700

    [Added] Next.js Tutorial #4 - Linking Between Pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Navbar from "../comps/Navbar";
 import Footer from "../comps/Footer";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -18,6 +19,9 @@ export default function Home() {
         nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
         volutpat.
       </p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   );
