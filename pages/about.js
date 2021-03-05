ad51cd7f08cce5c0971809e6348dfde08a66caaa
Hon <dennis.mejen@gmail.com>
2021-03-05 23:34:19 +0700

    [Added] Next.js Tutorial #2 - Pages & Routes

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,7 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-
-export default function Home() {
+const About = () => {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1>About</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
         nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
@@ -17,4 +14,6 @@ export default function Home() {
       </p>
     </div>
   );
-}
+};
+
+export default About;
